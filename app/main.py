be43ff3e381ad9a545ce258265690220bad9d585
wyk <wyk3393886@wsl.com>
2025-02-24 16:02:37 +0800

    test

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app.include_router(audio_router)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello, World"}
+    return {"message": "EchoStrings"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
